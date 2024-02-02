bb8742d644cb0d60c01e16c42c1fa62df3f225ab
KevinRamosCBLuna <107960388+KevinRamosCBLuna@users.noreply.github.com>
2024-02-02 14:49:14 -0800

    .

--- api/api_gateway.py
+++ api/api_gateway.py
@@ -47,5 +47,5 @@ def inventario_dashboards_function():
         return response
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8089))
+    port = int(os.environ.get("PORT", 8088))
     app.run(debug=True, host="0.0.0.0", port=port)
