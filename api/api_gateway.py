0753f97241d2b05a6c87c3d1b1486c783200df89
KevinRamosCBLuna <107960388+KevinRamosCBLuna@users.noreply.github.com>
2024-02-06 13:27:44 -0800

    cambios de puertos y añadir requirements

--- api/api_gateway.py
+++ api/api_gateway.py
@@ -47,5 +47,5 @@ def inventario_dashboards_function():
         return response
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8088))
+    port = int(os.environ.get("PORT", 8087))
     app.run(debug=True, host="0.0.0.0", port=port)
